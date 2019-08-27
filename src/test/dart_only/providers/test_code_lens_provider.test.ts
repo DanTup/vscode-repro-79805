7f17b61acc3d3eed729a36f0c9c7ff8349650298
Danny Tuppeny <danny@tuppeny.com>
2019-08-27 11:53:53 +0100

    More attempts

--- src/test/dart_only/providers/test_code_lens_provider.test.ts
+++ src/test/dart_only/providers/test_code_lens_provider.test.ts
@@ -13,7 +13,7 @@ describe(`test_code_lens`, () => {
 	console.info(`Starting tests!`);
 	before("activate", () => ext.activate());
 
-	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((attempt) => {
+	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].forEach((attempt) => {
 		it(`includes run/debug actions for tests (${attempt})`, async () => {
 			const doc = await vs.workspace.openTextDocument(helloWorldTestMainFile);
 			await vs.window.showTextDocument(doc);
