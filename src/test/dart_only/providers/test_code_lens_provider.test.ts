445c664869788816f4a8df1e5d05e1a55949e992
Danny Tuppeny <danny@tuppeny.com>
2019-08-27 10:07:11 +0100

    Update test_code_lens_provider.test.ts

--- src/test/dart_only/providers/test_code_lens_provider.test.ts
+++ src/test/dart_only/providers/test_code_lens_provider.test.ts
@@ -20,6 +20,7 @@ describe(`test_code_lens`, () => {
 			await delay(100);
 
 			const fileCodeLens = await getCodeLens(editor.document);
+			console.log(`Got ${fileCodeLens} code lens`);
 			debugCheck(fileCodeLens);
 		});
 	});
